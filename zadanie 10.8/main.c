fe7160943ec56f4193dc570984b73cfdf5212093
romanbeznosenko <245125@edu.p.lodz.pl>
2022-11-18 22:23:49 +0100

    Initial Commit

---
+++ zadanie 10.8/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  zadanie 10.8
+//
+//  Created by Роман Безносенко on 18/11/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
